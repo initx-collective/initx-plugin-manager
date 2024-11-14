462f1359d61424eae094c54c855f907620f48084
imba97 <mail@imba97.cn>
2024-11-14 20:23:22 +0800

    feat: list display effect

--- src/list.ts
+++ src/list.ts
@@ -1,14 +1,28 @@
 import path from 'node:path'
 import fs from 'fs-extra'
+
 import { fetchPlugins } from '@initx-plugin/core'
-import { blueBright, gray } from 'picocolors'
+
+import columnify from 'columnify'
+import { gray } from 'picocolors'
+
+import { nameColor } from './utils'
+import type { PluginInfo } from './types'
 
 export async function showPluginList() {
   const plugins = await fetchPlugins()
 
+  const displayTable: PluginInfo[] = []
+
   plugins.forEach(({ root }) => {
     const info = fs.readJsonSync(path.join(root, 'package.json'))
-    // eslint-disable-next-line no-console
-    console.log(`${blueBright(info.name)}\t${gray(info.version)}\t${gray(info.description)}`)
+    displayTable.push({
+      name: nameColor(info.name),
+      version: gray(info.version),
+      description: gray(info.description)
+    })
   })
+
+  // eslint-disable-next-line no-console
+  console.log(columnify(displayTable))
 }
